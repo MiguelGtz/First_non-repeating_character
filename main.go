735c0c8c189418e6927142029cae725e02890327
MiguelGutierrez <imickey2010@hotmail.com>
2020-11-12 16:25:46 -0600

    Programa Terminado!

--- main.go
+++ main.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func firstNonRepeating(str string) string {
+	minusculas := strings.ToLower(str)
+	for i := range minusculas {
+		if strings.Count(minusculas,minusculas[i:i+1]) == 1 {
+			return str[i:i+1]
+		}
+	}
 	return ""
 }
 
 func main() {
-	fmt.Println(firstNonRepeating("hola"))
+	fmt.Println(firstNonRepeating("sTreSS"))
+	fmt.Println(firstNonRepeating("Go hang a salami, I'm a lasagna hog!"))
 }
